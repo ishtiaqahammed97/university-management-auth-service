9716be9b61465733ac41d5e0bfa7440a6d08828f
ishtiaq <isah2080@ymail.com>
2023-08-03 19:42:47 +0600

    [DONE] setup express, mongoose, cors, parser

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+// dotenv.config({path: process.cwd()}) // cwd - current directory
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+
+export default {
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL
+}
