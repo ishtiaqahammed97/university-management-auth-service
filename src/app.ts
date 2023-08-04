a28b3728581b541485523a6b5bd0fb19241b4016
ishtiaq <isah2080@ymail.com>
2023-08-04 16:46:10 +0600

    [DONE] setup eslint prettier husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
 import express, { Application, Request, Response } from 'express'
 const app: Application = express()
-const port = 3000
-import cors from 'cors';
+// const port = 3000
+import cors from 'cors'
 
-
-app.use(cors());
+app.use(cors())
 
 // parser
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
+app.use(express.urlencoded({ extended: true }))
 
 // Testing purpose
 app.get('/', (req: Request, res: Response) => {
-    res.send('Working successfully')
+  res.send('Working successfully')
 })
 
-export default app;
+export default app
