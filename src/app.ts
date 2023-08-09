58f3a784850c784705c61e89e7aa0fe2bc323d56
ishtiaq <isah2080@ymail.com>
2023-08-09 20:58:55 +0600

    [DONE] User service, create user route

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 // const port = 3000
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 
 app.use(cors())
 
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Application routes
+app.use('/api/v1/users/', usersRouter)
 
 // Testing purpose
 app.get('/', (req: Request, res: Response) => {
