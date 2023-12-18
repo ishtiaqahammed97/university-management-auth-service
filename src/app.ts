8a8d62d013228fd7a54d924eaad4ed1114912581
ishtiaq <isah2080@ymail.com>
2023-12-18 11:57:55 +0600

    [DONE] academic semester, catchAsync & response code refactor

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 const app: Application = express()
 // const port = 3000
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import routes from './app/routes'
+import httpStatus from 'http-status'
 
 app.use(cors())
 
@@ -21,6 +22,21 @@ app.use('/api/v1/', routes)
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
 //   throw new Error('Testing error logger')
 // })
+
+// handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessage: [
+      {
+        path: '.',
+        message: 'API not Found',
+      },
+    ],
+  })
+  next()
+})
 
 // global error handler
 app.use(globalErrorHandler)
