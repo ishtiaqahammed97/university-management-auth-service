2f5795727942f5300801b4c848250ac580968483
ishtiaq <isah2080@ymail.com>
2023-12-22 21:50:00 +0600

    [DONE] create academic department with zod validation

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose'
+import {
+  IAcademicDepartment,
+  IAcademicDepartmentModel,
+} from './academicDepartment.interface'
+
+const AcademicDepartmentSchema = new Schema<
+  IAcademicDepartment,
+  IAcademicDepartmentModel
+>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+      required: true,
+    },
+  },
+  // created at and updated at
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+)
+
+export const AcademicDepartment = model<
+  IAcademicDepartment,
+  IAcademicDepartmentModel
+>('AcademicDepartment', AcademicDepartmentSchema)
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,21 +2,26 @@ import express from 'express'
 import { UserRoutes } from '../modules/user/user.route'
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route'
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route'
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route'
 
 const router = express.Router()
 
 const moduleRoutes = [
   {
-    path: '/users/',
+    path: '/users',
     route: UserRoutes,
   },
   {
-    path: '/academic-semesters/',
+    path: '/academic-semesters',
     route: AcademicSemesterRoutes,
   },
   {
-    path: '/academic-faculty/',
+    path: '/academic-faculty',
     route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-department',
+    route: AcademicDepartmentRoutes,
   },
 ]
 
