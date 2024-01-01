05b3903584557ee1bce5afdf21f7758dfca6b3fb
ishtiaq <isah2080@ymail.com>
2024-01-01 12:24:17 +0600

    admin create with validation

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,39 +1,58 @@
-import { Request, Response } from 'express'
+import { Request, RequestHandler, Response } from 'express'
 import { UserService } from './user.service'
 import catchAsync from '../../../shared/catchAsync'
 import sendResponse from '../../../shared/sendResponse'
 import httpStatus from 'http-status'
 
-const createStudent = catchAsync(async (req: Request, res: Response) => {
-  // req.body
-  // {
-  //   password: ,
-  //   student / faculty / admin
-  // }
-  const { student, ...usersData } = req.body
-  const result = await UserService.createStudent(student, usersData)
+const createStudent: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    // req.body
+    // {
+    //   password: ,
+    //   student / faculty / admin
+    // }
+    const { student, ...usersData } = req.body
+    const result = await UserService.createStudent(student, usersData)
 
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'User Created Successfully',
-    data: result,
-  })
-})
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'User Created Successfully',
+      data: result,
+    })
+  },
+)
 
-const createFaculty = catchAsync(async (req: Request, res: Response) => {
-  const { faculty, ...userData } = req.body
-  const result = await UserService.createFaculty(faculty, userData)
+const createFaculty: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { faculty, ...userData } = req.body
+    const result = await UserService.createFaculty(faculty, userData)
 
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Faculty Created Successfully',
-    data: result,
-  })
-})
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Faculty Created Successfully',
+      data: result,
+    })
+  },
+)
+
+const createAdmin: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { admin, ...userData } = req.body
+    const result = await UserService.createAdmin(admin, userData)
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Admin Created Successfully',
+      data: result,
+    })
+  },
+)
 
 export const UserController = {
   createStudent,
   createFaculty,
+  createAdmin,
 }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -5,12 +5,18 @@ import ApiError from '../../../errors/ApiError'
 import { IUser } from './user.interface'
 import { User } from './user.model'
 import { AcademicSemester } from '../academicSemester/academicSemester.model'
-import { generateFacultyId, generateStudentId } from './user.utils'
+import {
+  generateAdminId,
+  generateFacultyId,
+  generateStudentId,
+} from './user.utils'
 import mongoose from 'mongoose'
 import { Student } from '../student/student.model'
 import httpStatus from 'http-status'
 import { Faculty } from '../faculty/faculty.model'
 import { IFaculty } from '../faculty/faculty.interface'
+import { IAdmin } from '../admin/admin.interface'
+import { Admin } from '../admin/admin.model'
 // import { generateFacultyId } from './user.utils'
 
 const createStudent = async (
@@ -155,7 +161,67 @@ const createFaculty = async (
   return newUserAllData
 }
 
+const createAdmin = async (
+  admin: IAdmin,
+  user: IUser,
+): Promise<IUser | null> => {
+  // default password
+  if (!user.password) {
+    user.password = config.default_admin_pass as string
+  }
+
+  // set role
+  user.role = 'admin'
+
+  // generate faculty id
+  let newUserAllData = null
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+
+    const id = await generateAdminId()
+    user.id = id
+    admin.id = id
+
+    const newAdmin = await Admin.create([admin], { session })
+
+    if (!newAdmin.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to admin')
+    }
+
+    user.admin = newAdmin[0]._id
+
+    const newUser = await User.create([user], { session })
+
+    if (!newUser.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to create admin')
+    }
+    newUserAllData = newUser[0]
+
+    await session.commitTransaction()
+    await session.endSession()
+  } catch (error) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw error
+  }
+
+  if (newUserAllData) {
+    newUserAllData = await User.findOne({ id: newUserAllData.id }).populate({
+      path: 'admin',
+      populate: [
+        {
+          path: 'managementDepartment',
+        },
+      ],
+    })
+  }
+
+  return newUserAllData
+}
+
 export const UserService = {
   createStudent,
   createFaculty,
+  createAdmin,
 }
